5f2c25edec714852d29c5b3e6af64beb7eb2561d
Gui-dev <adrianon10@gmail.com>
2022-04-08 03:58:41 -0300

    feat: add connection with firestore

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,27 +1,27 @@
 import { initializeApp } from 'firebase/app'
-import { getDatabase } from 'firebase/database'
-import { getAnalytics } from 'firebase/analytics'
-import {
-  FIREBASE_API_KEY,
-  FIREBASE_AUTH_DOMAIN,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET,
-  FIREBASE_APP_ID,
-  FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_MEASUREMENT_ID
-} from 'react-native-dotenv'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+// import {
+//   FIREBASE_API_KEY,
+//   FIREBASE_AUTH_DOMAIN,
+//   FIREBASE_PROJECT_ID,
+//   FIREBASE_STORAGE_BUCKET,
+//   FIREBASE_APP_ID,
+//   FIREBASE_MESSAGING_SENDER_ID,
+//   FIREBASE_MEASUREMENT_ID
+// } from 'react-native-dotenv'
 
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_APP_ID,
-  appId: FIREBASE_MESSAGING_SENDER_ID,
-  measurementId: FIREBASE_MEASUREMENT_ID
+  apiKey: 'AIzaSyCGxZ_2whs_OrTOnwDK5XTwET5JoPem7To',
+  authDomain: 'finance-32f77.firebaseapp.com',
+  projectId: 'finance-32f77',
+  storageBucket: 'finance-32f77.appspot.com',
+  messagingSenderId: '218463812867',
+  appId: '1:218463812867:web:4114383bf7cd203ee1bd36',
+  measurementId: 'G-LTJPR6LFK7'
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-export const database = getDatabase(app)
-export const analytics = getAnalytics(app)
+export const database = getFirestore(app)
+export const firebaseAuth = getAuth(app)
