4055d4e28a50d8237c77a924de9e75a2ce079d12
Gui-dev <adrianon10@gmail.com>
2022-04-14 21:09:44 -0300

    style: modifying position

--- src/components/ButtonHamburger/style.ts
+++ src/components/ButtonHamburger/style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components/native'
 export const Container = styled.View`
   position: absolute;
   top: 40px;
-  right: 40px;
+  right: 20px;
 `
 
 export const ButtonToggle = styled.TouchableWithoutFeedback`
