7f35bb92c073ccd91be342628ed56346928cdce7
Gui-dev <adrianon10@gmail.com>
2022-04-20 04:17:48 -0300

    feat: add helper function handleSubmit

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,5 +1,12 @@
 import React, { useState } from 'react'
-import { Keyboard, KeyboardAvoidingView, Platform } from 'react-native'
+import { Alert, Keyboard, KeyboardAvoidingView, Platform, ActivityIndicator } from 'react-native'
+import { collection, doc, getDoc, setDoc, updateDoc } from 'firebase/firestore'
+import { useNavigation } from '@react-navigation/native'
+import uuid from 'react-native-uuid'
+import { DrawerNavigationProp } from '@react-navigation/drawer'
+
+import { firebaseAuth, database } from './../../services/firebase'
+import { AppRoutesParamList } from './../../routes/app.routes'
 
 import { COLORS } from '../../themes'
 import { Picker } from './../../components/Picker'
@@ -7,11 +14,58 @@ import { Background } from '../../components/Background'
 import { ButtonHamburger } from '../../components/ButtonHamburger'
 import { Container, Content, InputValue, SubmitButton, SubmitButtonText } from './style'
 
+type RegisterScreenNavigationProps = DrawerNavigationProp<AppRoutesParamList>
+
+type UserProps = {
+  name: string
+  email: string
+  balance: number
+}
+
 export const Register = () => {
   const [value, setValue] = useState('')
   const [type, setType] = useState('')
+  const [loading, setLoading] = useState(false)
+  const { navigate } = useNavigation<RegisterScreenNavigationProps>()
 
-  console.log(type)
+  const handleSubmit = async () => {
+    try {
+      setLoading(true)
+      const uid = firebaseAuth.currentUser?.uid
+
+      if (value === '' || type === '') {
+        Alert.alert('Opssss', 'Preencha todos os campos')
+        return
+      }
+
+      const docCollection = collection(database, 'movements')
+      const docRef = doc(docCollection, String(uuid.v4()))
+
+      await setDoc(docRef, {
+        id: uid,
+        type,
+        value,
+        created_at: new Date()
+      })
+
+      const usersCollection = collection(database, 'users')
+      const userRef = doc(usersCollection, uid)
+      const userUpdated = await getDoc(userRef)
+      const user = userUpdated.data() as UserProps
+
+      await updateDoc(userRef, {
+        balance: type === 'recipe' ? user.balance += Number(value) : user.balance -= Number(value)
+      })
+
+      setValue('')
+      setType('')
+      navigate('Home')
+    } catch {
+      Alert.alert('Opssss', 'Erro ao atualizar movimentações')
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
     <Background>
@@ -37,8 +91,12 @@ export const Register = () => {
 
             <Picker type={type} onSetType={setType} />
 
-            <SubmitButton>
-              <SubmitButtonText>Registrar</SubmitButtonText>
+            <SubmitButton onPress={handleSubmit}>
+              {
+                loading
+                  ? <ActivityIndicator size="large" color={COLORS.WHITE}/>
+                  : <SubmitButtonText>Registrar</SubmitButtonText>
+              }
             </SubmitButton>
           </Content>
         </Container>
