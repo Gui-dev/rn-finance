537d691498aef55ba5a9e35edf27e7154a73dd10
Gui-dev <adrianon10@gmail.com>
2022-04-29 01:04:30 -0300

    feat: add environment variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,24 +1,15 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
-// import {
-//   FIREBASE_API_KEY,
-//   FIREBASE_AUTH_DOMAIN,
-//   FIREBASE_PROJECT_ID,
-//   FIREBASE_STORAGE_BUCKET,
-//   FIREBASE_APP_ID,
-//   FIREBASE_MESSAGING_SENDER_ID,
-//   FIREBASE_MEASUREMENT_ID
-// } from 'react-native-dotenv'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCGxZ_2whs_OrTOnwDK5XTwET5JoPem7To',
-  authDomain: 'finance-32f77.firebaseapp.com',
-  projectId: 'finance-32f77',
-  storageBucket: 'finance-32f77.appspot.com',
-  messagingSenderId: '218463812867',
-  appId: '1:218463812867:web:4114383bf7cd203ee1bd36',
-  measurementId: 'G-LTJPR6LFK7'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
